a6c28b06af2e991350ae871d2a171656884c5c7d
Alberto Silva <albertos092@uol.com.br>
2022-02-19 21:44:26 -0300

    correction parentheses

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,5 +1,6 @@
 # write your code here
 import random
+
 print('Enter the number of friends joining (including you):')
 number_of_friends = int(input())
 if number_of_friends > 0:
@@ -7,9 +8,9 @@ if number_of_friends > 0:
     print('Enter the name of every friend (including you), each on a new line')
     for i in range(number_of_friends):
         name_friend = input()
-        
+
         friends[name_friend] = 0
-    #print(friends)
+    # print(friends)
     print()
     print('Enter the total bill value:')
     total_bill = float(input())
@@ -30,4 +31,3 @@ if number_of_friends > 0:
     print(friends)
 else:
     print('No one is joining for the party')
-
